ff12a068d4a652aa4ecb5667dce0c1ca9e2977a9
seabasshoang <sylviahoang@hotmail.co.uk>
2019-02-26 14:35:51 +0000

    Updates

---
+++ precache-manifest.5f3e3cd63d7f714b1fff16bf539a2153.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shop-the-story/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8d70382bf9a79262de08",
+    "url": "/shop-the-story/static/js/runtime~main.8d70382b.js"
+  },
+  {
+    "revision": "30b3c77959bdfcd004e9",
+    "url": "/shop-the-story/static/js/main.30b3c779.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/shop-the-story/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "30b3c77959bdfcd004e9",
+    "url": "/shop-the-story/static/css/main.1d6e9679.chunk.css"
+  },
+  {
+    "revision": "338c8f35c4855acc3bbe7eeac3b67514",
+    "url": "/shop-the-story/index.html"
+  }
+];
