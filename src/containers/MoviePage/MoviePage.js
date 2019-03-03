afd32e1ceb5f6ee165a306cf551869b325a1182e
seabasshoang <sylviahoang@hotmail.co.uk>
2019-03-03 19:54:36 +0000

    added flower movie stills

--- src/containers/MoviePage/MoviePage.js
+++ src/containers/MoviePage/MoviePage.js
@@ -1,17 +1,99 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 import { Link } from 'react-router-dom';
 import './MoviePage.css';
-import movieIMG from '../../assets/images/movieSC.png';
 import FooterLogo from '../../components/Footer/FooterLogo';
-//import SmallLogo from '../../components/SmallLogo/SmallLogo';
 import movietitle from '../../assets/images/movietitle.png';
+import flower1 from '../../assets/images/flower1.png';
+import flower2 from '../../assets/images/flower2.png';
+import flower3 from '../../assets/images/flower3.png';
+import flower4 from '../../assets/images/flower4.png';
+import flower5 from '../../assets/images/flower5.png';
 
-const MoviePage = () => (
-    <div className="moviePageContainer">
-        <img src={movietitle} className="movieTitle" alt='flower-arrangement-class'/>
-        <Link to="productspage"><div className="movie"><img src={movieIMG} alt="movieIMG"></img></div></Link>
+class MoviePage extends Component {
+
+    constructor(props, context) {
+        super(props, context);
+    
+        this.handleSelect = this.handleSelect.bind(this);
+    
+        this.state = {
+          index: 0,
+          direction: null,
+        };
+      }
+    
+      handleSelect(selectedIndex, e) {
+        this.setState({
+          index: selectedIndex,
+          direction: e.direction,
+        });
+      }
+    
+      render() {
+        const { index, direction } = this.state;
+    
+        return (
+            <div className="moviePageContainer">
+            <img src={movietitle} className="movieTitle" alt='flower-arrangement-class'/>
+          {/* <Carousel
+            activeIndex={index}
+            direction={direction}
+            onSelect={this.handleSelect}
+          >
+            <Carousel.Item>
+                    <img src={flower1} alt="flower1"/>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img src={flower2} alt="flower2"/>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img src={flower3} alt="flower3"/>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img src={flower4} alt="flower4"/>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img src={flower5} alt="flower5"/>
+                </Carousel.Item>
+          </Carousel> */}
+          <FooterLogo/>
+        </div>
+        );
+      }
+    }
+    
+
+
+
+//     render() {
         
-        <FooterLogo/>
-    </div>
-);
+//         return (
+            // <div className="moviePageContainer">
+            // <img src={movietitle} className="movieTitle" alt='flower-arrangement-class'/>
+//             <Carousel>
+                // <Carousel.Item>
+                //     <img src={flower1} alt="flower1"/>
+                // </Carousel.Item>
+                // <Carousel.Item>
+                //     <img src={flower2} alt="flower2"/>
+                // </Carousel.Item>
+                // <Carousel.Item>
+                //     <img src={flower3} alt="flower3"/>
+                // </Carousel.Item>
+                // <Carousel.Item>
+                //     <img src={flower4} alt="flower4"/>
+                // </Carousel.Item>
+                // <Carousel.Item>
+                //     <img src={flower5} alt="flower5"/>
+                // </Carousel.Item>
+//                 </Carousel>
+        
+        //     <FooterLogo/>
+        // </div>
+//         )
+//     }
+// }
+
+
 export default MoviePage;
